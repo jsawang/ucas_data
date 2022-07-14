bc53342436cfb3e73f1621d35d01ac8a27f941ae
Jenni Awang <108807269+jsawang@users.noreply.github.com>
2022-07-14 22:49:41 +0100

    Add barchart by age and gender

--- chart_replications.R
+++ chart_replications.R
@@ -31,3 +31,21 @@ ggplot(data = apps_filter,
   scale_x_continuous(breaks=seq(2006,2022,1)) +
   guides(x = guide_axis(n.dodge = 2)) +
   labs(title = "Applicants by age")
+
+apps_indage <- apps_agegp[apps_agegp$Domicile == filter_apptype &
+                            apps_agegp$Gender != filter_gender &
+                            apps_agegp$`Applicant type` == filter_apptype &
+                            apps_agegp$`Individual age` != filter_indivage &
+                            apps_agegp$`Age group` != filter_agegp,]
+
+apps_indage$`Individual age` <- as.factor(apps_indage$`Individual age`)
+
+ggplot(data = apps_indage,
+       aes(y = `Individual age`,
+           x = Applicants,
+           fill = Gender)
+) +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  labs(title = "Applicants by individual age and gender") +
+  scale_y_discrete(limits = rev(levels(apps_indage$`Individual age`))) +
+  scale_x_continuous(labels = scales::number)
